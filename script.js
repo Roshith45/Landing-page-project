f22f971239f31c852fd719b23c75ad3ecb3d0c4c
Roshith45 <146621906+Roshith45@users.noreply.github.com>
2024-08-08 14:54:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Example JavaScript functionality (toggle navigation menu)
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('nav ul');
+
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+});
